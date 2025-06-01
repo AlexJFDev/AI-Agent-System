1c6b28015f0b3df99e27ce5b3acf660b9d16997b
alex <alexjfuhr@gmail.com>
2025-06-01 17:35:20 -0400

    Changed model

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ from games import GAME_IN_FILEPATH, GAME_OUT_FILEPATH
 
 load_dotenv()
 
+REGULAR = "gpt-4.1"
+MINI = "gpt-4.1-mini"
+NANO = "gpt-4.1-nano"
+
 CHATGPT_KEY = os.getenv("OPEN_AI_KEY")
 
 def main():
     environment = IOEnvironment(GAME_IN_FILEPATH, GAME_OUT_FILEPATH)
-    chatgpt = ChatGPTEngine(CHATGPT_KEY)
+    chatgpt = ChatGPTEngine(CHATGPT_KEY, model=MINI)
     memory = ReasoningMemoryDatabase(chatgpt, file_path=f"memories_{time.time()}.json")
 
     agent = OregonAgent(environment, chatgpt, memory)
