5761b1e4e37d4d1ee154a909fb9eb8d2c3e1e0e6
alex <alexjfuhr@gmail.com>
2025-06-01 22:47:41 -0400

    Updated oregon trail implementation

--- games/oregon_trail.py
+++ games/oregon_trail.py
@@ -2,15 +2,6 @@ import random
 import random
 import time
 
-import sys
-from game_io import GameIO
-
-GAME_IN_FILEPATH = "game_in.txt"
-GAME_OUT_FILEPATH = "game_out.txt"
-
-sys.stdin = GameIO(GAME_IN_FILEPATH)
-sys.stdout = GameIO(GAME_OUT_FILEPATH)
-
 def setup():
     # set up the variables to be used in game
     game_variables = {
